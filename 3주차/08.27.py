7fb31976448bc23e32af2a8d31f3254b14f00ec5
jongdu <parksn5029@naver.com>
2024-08-27 19:58:22 +0900

    24.08.27 정렬 문제 풀이

---
+++ 3주차/08.27.py
@@ -0,0 +1,55 @@
+#점수 계산
+# array = []
+
+# for _ in range(8):
+#     array.append(int(input()))
+    
+# array_sort = sorted(array, reverse=True)
+
+# sum = sum(array_sort[:5])
+# print(sum)
+
+# result = []
+# for i in range(5):
+#     result.append(array.index(array_sort[i])+1)
+
+# for i in sorted(result):
+#     print(i, end=" ")
+    
+#카드
+# N = int(input())
+# r = dict()
+# for _ in range(N):
+#     n = int(input())
+#     if n in r:
+#         r[n] += 1
+#     else:
+#         r[n] = 1
+
+# sort_r = sorted(r.items(), key=lambda x: (-x[1], x[0]))
+# print(sort_r[0][0])
+
+#두 수의 합
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+array = list(map(int, input().split()))
+array.sort()
+find = int(input())
+
+start = 0
+end = N-1
+
+cnt = 0
+while start<end:
+    if array[start] + array[end] == find:
+        start +=1
+        end -=1
+        cnt +=1
+    elif array[start] + array[end] > find:
+        end -=1
+    else:
+        start +=1
+        
+print(cnt)
